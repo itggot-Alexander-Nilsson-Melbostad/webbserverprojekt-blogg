7b2dfde98fbcae87b26d3e5a944d853067d208fc
itggot-Alexander-Nilsson-Melbostad <Alexander.nilssonmelbostad@itggot.se>
2019-02-07 18:04:00 +0100

    /new och /loginfailed
    Lägger till lite nya routes till appen som gör att jag kan skapa ett fungerande loginsystem och även så att man ska kunna skapa en ny användare och få in de hashade lösenorden direkt i databasen

--- app.rb
+++ app.rb
@@ -2,4 +2,53 @@ require 'slim'
 require 'sinatra'
 require 'SQLite3'
 require 'BCrypt'
+require 'byebug'
 
+enable :sessions
+
+get("/index") do
+    slim(:index)
+end
+
+post("/login") do
+    db = SQLite3::Database.new("db(blogg.db")
+    db.results_as_hash = true
+    result = db.execute("SELECT Username, Password, Authority FROM users WHERE Username = '#{params["Username"]}'")
+    if BCrypt::Password.new(result[0]["Password"]) == params["Password"]
+        session[:User] = params["Username"]
+    else
+        redirect("/loginfailed")
+    end
+    slim(:index, locals:{
+        index: result
+    })
+    redirect("/")
+end
+
+post("/logout") do
+    session.destroy
+    redirect("/")
+end
+
+get("/loginfailed") do
+    slim(:loginfailed)
+end
+
+post("/create") do
+    db =SQLite3::Database.new("db/blogg.db") do
+    db.results_as_hash = true
+    new_name = params["Username"] 
+    new_password = ["Password1"]
+
+    if params["Password1"] == params["Password2"]
+        new_password_hash = BCrypt::Password.create(new_password)
+        db.execute("INSERT INTO users (Username, Password, UserId, Authority, Nickname) VALUES (?,?,?,?,?)", new_name, new_password,new_userid, 1, new_nickname)
+        redirect("/posts")
+    else
+        redirect("/loginfailed")
+    end
+end
+
+get("/new") do
+    slim(:new)
+end     
